5e913dbb03cc0c6a2cd151bc04adc98e9a210b15
tom-schultz-robecosam <108812925+tom-schultz-robecosam@users.noreply.github.com>
2022-07-11 11:30:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.header('💋Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
