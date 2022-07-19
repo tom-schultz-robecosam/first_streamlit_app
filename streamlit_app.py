829242a51cb3e5a3efdc97002f04a4264d15b6dc
tom-schultz-robecosam <108812925+tom-schultz-robecosam@users.noreply.github.com>
2022-07-19 12:08:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,3 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
