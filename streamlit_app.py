78f99ac7bad1e2185603e7be23c3e384b530f6f2
tom-schultz-robecosam <108812925+tom-schultz-robecosam@users.noreply.github.com>
2022-07-19 12:07:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.header('💋Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -19,3 +20,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
